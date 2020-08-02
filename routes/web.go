c420036b071b4a8ecf15abe7025c4de8539e8def
Tran Xuan Nam <nam@nimblehq.co>
2020-08-02 22:40:17 +0700

    As a user I can sign out

--- routes/web.go
+++ routes/web.go
@@ -13,10 +13,15 @@ func registerWeb(r *ginutils.ApplicationRouter, mids ...gin.HandlerFunc) {
 	r.Middlewares(mids...)
 
 	r.Register("GET", "/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{})
+	})
+
+	r.Register("GET", "/dashboard", middlewares.Authenticated(), func(c *gin.Context) {
 		c.JSON(http.StatusOK, c.Keys["current_user"])
 	})
 
 	r.Register("GET", "/oauth2/callback", controllers.LoginCallback)
 
 	r.Register("GET", "/login", middlewares.Guest(), controllers.Login)
+	r.Register("POST", "/logout", middlewares.Authenticated(), controllers.Logout)
 }
