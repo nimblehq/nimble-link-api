9cb4271f83a7b7e99ad56b0e78a769ae91792bb1
Tran Xuan Nam <nam@nimblehq.co>
2020-08-14 17:33:39 +0700

    Config CORS

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"strings"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/nimble-link/backend/middlewares"
@@ -14,7 +16,9 @@ func Register(g *gin.Engine) *gin.Engine {
 	config := cors.DefaultConfig()
 	config.AddAllowHeaders("X-Requested-With")
 	config.AddAllowHeaders("Authorization")
-	config.AllowAllOrigins = true
+	config.AllowOriginFunc = func(origin string) bool {
+		return strings.HasPrefix(origin, "http://localhost") || strings.HasSuffix(origin, "nimble-link.vercel.app")
+	}
 	g.Use(cors.New(config))
 
 	g.Use(middlewares.CurrentUser())
